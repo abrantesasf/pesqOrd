106ca6cbe94b8eb7ad6f45aac384e87751340d36
abrantesasf <abrantesasf@gmail.com>
2019-11-15 11:29:53 -0300

    Implementação da aleatorização utilizando a classe StdRandom padronizada por Sedgewick e Wayne, no livro Algorithms.

--- java/PesqOrd/src/classes/Quick.java
+++ java/PesqOrd/src/classes/Quick.java
@@ -1,5 +1,7 @@
 package classes;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 /**
  * <p>A classe <code><b>Quick</b></code> implementa o método de ordenação <b>Quicksort</b>
  * que será utilizado no trabalho final da disciplina de Pesquisa e Ordenação,
@@ -10,7 +12,7 @@ package classes;
  * <li><b>SEDGEWICK R, WAYNE K. Algorithms. 4 ed. Princeton: Addison Wesley, 2011.</b></li>
  * </ul>
  * 
- * @author Abrantes Araújo Silva Filho ({@link<a href="mailto:abrantesasf@gmail.com">abrantesasf@gmail.com</a>})
+ * @author Abrantes Araújo Silva Filho (<a href="mailto:abrantesasf@gmail.com">abrantesasf@gmail.com</a>)
  */
 public class Quick {
 	
@@ -30,11 +32,12 @@ public class Quick {
 	
 	
     ///////////////////////////////////////////////////
-    // Quicksort
+    // Quicksort: implementação de Sedgewick
     ///////////////////////////////////////////////////
 	
 	/**
-	 * <p>O método <code><b>sort</b></code> implementa o Quicksort, que é um
+	 * <p>O método <code><b>sort</b></code> implementa o Quicksort (conforme
+	 * os autores Sedgewick e Wayne), que é um
 	 * método de ordenação "in-place", custo proporcional a nlog(n) na média,
 	 * e utiliza uma estratégia de dividir e conquistar.</p>
 	 * 
@@ -49,6 +52,7 @@ public class Quick {
 	 */
 	@SuppressWarnings("rawtypes")
 	public static void sort(Comparable[] a) {
+		StdRandom.shuffle(a);
 		sort(a, 0, a.length - 1);
     }
 	
@@ -56,8 +60,8 @@ public class Quick {
 	private static void sort(Comparable[] a, int lo, int hi) {
 		if (hi <= lo) return;
 		int j = partition(a, lo, hi);
-		sort(a, lo, j-1);
-		sort(a, j+1, hi);
+		sort(a, lo , j-1);
+		sort(a, j+1, hi );
 	}
 	
 	@SuppressWarnings("rawtypes")
@@ -78,6 +82,98 @@ public class Quick {
 	
 	
 	
+    ///////////////////////////////////////////////////
+    // Quicksort: implementação de Cormen
+    ///////////////////////////////////////////////////	
+	
+	/**
+	 * <p>O método <code><b>sortCLRS</b></code> implementa o Quicksort (conforme
+	 * os autores Cormen, Leiserson, Rivest e Stein), que é um
+	 * método de ordenação "in-place", custo proporcional a nlog(n) na média,
+	 * e utiliza uma estratégia de dividir e conquistar.</p>
+	 * 
+	 * <p>O Quicksort funciona <b>particionando</b> um array em 2 sub-arrays,
+	 * e então ordenando os sub-arrays de modo independente.
+	 * 
+	 * @param a
+	 *        (Comparable[] a) este método recebe como parâmetro um vetor de
+	 *        objetos que implementam a interface <code>Comparable</code> com
+	 *        os critérios de ordenação. De acordo com as instruções do trabalho,
+	 *        as contas serão ordenadas por CPF e conta.
+	 */
+	@SuppressWarnings("rawtypes")
+	public static void sortCLRS(Comparable[] a) {
+		sortCLRS(a, 0, a.length - 1);
+    }
+	
+	@SuppressWarnings("rawtypes")
+	private static void sortCLRS(Comparable[] a, int p, int r) {
+		if (p < r) {
+			int q = partitionCLRS(a, p, r);
+			sortCLRS(a, p  , q-1);
+			sortCLRS(a, q+1, r  );
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private static int partitionCLRS(Comparable[] a, int p, int r) {
+		Comparable x = a[r];
+		int i = p-1;
+		
+		for (int j = p; j < r; j++) {
+			if ( lessCLRS( a[j], x ) ) {
+				i = i + 1;
+				exchange(a, i, j);
+			}
+		}
+		exchange(a, i+1, r);
+		return i+1;
+	}
+	
+	
+	
+    ///////////////////////////////////////////////////
+    // Quicksort: implementação randomizada de Cormen
+    ///////////////////////////////////////////////////	
+	
+	/**
+	 * <p>O método <code><b>sortCLRSrand</b></code> implementa o Quicksort randomizado (conforme
+	 * os autores Cormen, Leiserson, Rivest e Stein), que é um
+	 * método de ordenação "in-place", custo proporcional a nlog(n) na média,
+	 * e utiliza uma estratégia de dividir e conquistar.</p>
+	 * 
+	 * <p>O Quicksort funciona <b>particionando</b> um array em 2 sub-arrays,
+	 * e então ordenando os sub-arrays de modo independente.
+	 * 
+	 * @param a
+	 *        (Comparable[] a) este método recebe como parâmetro um vetor de
+	 *        objetos que implementam a interface <code>Comparable</code> com
+	 *        os critérios de ordenação. De acordo com as instruções do trabalho,
+	 *        as contas serão ordenadas por CPF e conta.
+	 */
+	@SuppressWarnings("rawtypes")
+	public static void sortCLRSrand(Comparable[] a) {
+		sortCLRSrand(a, 0, a.length - 1);
+    }
+	
+	@SuppressWarnings("rawtypes")
+	private static void sortCLRSrand(Comparable[] a, int p, int r) {
+		if (p < r) {
+			int q = partitionCLRSrand(a, p, r);
+			sortCLRSrand(a, p  , q-1);
+			sortCLRSrand(a, q+1, r  );
+		}
+	}
+	
+	@SuppressWarnings("rawtypes")
+	private static int partitionCLRSrand(Comparable[] a, int p, int r) {
+		int i = p + (int) Math.random() % (r - p + 1);
+		exchange(a, r, i);
+		return partitionCLRS(a, p, r);
+	}	
+	
+	
+	
 	///////////////////////////////////////////////////
     // Métodos auxiliares privados
     ///////////////////////////////////////////////////	
@@ -86,6 +182,12 @@ public class Quick {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private static boolean less(Comparable v, Comparable w) {
 		return v.compareTo(w) < 0;
+	}
+	
+	/** <p>Realiza a comparação durante a ordenação</p> **/
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	private static boolean lessCLRS(Comparable v, Comparable w) {
+		return v.compareTo(w) <= 0;
 	}
 	
 	/** <p>Troca dois objetos de posição.</p> **/
